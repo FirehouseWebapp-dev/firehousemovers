8a8bf9f8980fe55e6f8e26d9850237796fac2ea9
FirehouseMovers dev <github@firehousemovers.com>
2025-08-14 02:16:43 +0500

    Removed unnecessary changes 5

--- authentication/views.py
+++ authentication/views.py
@@ -331,7 +331,7 @@ def add_team_member(request):
         form = AddTeamMemberForm()
     return render(request, 'authentication/add_member.html', {'form': form})
 
-    
+
 from django.views.decorators.http import require_POST
 
 @login_required
